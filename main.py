c6b7387e42a0c584781cf33b1cc45b3df04ba991
SWASTIKA BARMAN <155806171+swastikabarman@users.noreply.github.com>
2026-02-10 03:10:37 +0530

    Modify upload_video to include description field
    Added description parameter to video upload endpoint.

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
-#Accept video from teacher
-# Call S3 upload logic
-# Return URL
-
 from fastapi import FastAPI, UploadFile, File, Form
 from s3_service import upload_video_to_s3
 from database.upload import insert_video
 
 app = FastAPI()
 
+
 @app.post("/upload-video")
-def upload_video(
+async def upload_video(
     file: UploadFile = File(...),
     title: str = Form(...),
     subject: str = Form(...),
-    teacher_id: int = Form(...)
+    teacher_id: int = Form(...),
+    description: str = Form(None)
 ):
     # 1. Upload video to S3
     video_url = upload_video_to_s3(file)
 
-    # 2. Save metadata in SQL Server
+    # 2. Save metadata
     insert_video(
         title=title,
         subject=subject,
         video_url=video_url,
-        teacher_id=teacher_id
+        teacher_id=teacher_id,
+        description=description
     )
 
     return {
         "message": "Video uploaded and saved successfully",
         "video_url": video_url
     }
+
+
+
