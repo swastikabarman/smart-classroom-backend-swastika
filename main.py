aa3c0e9f6450af9bf7446af5f35b784ad7c51161
SWASTIKA BARMAN <155806171+swastikabarman@users.noreply.github.com>
2026-02-14 21:56:01 +0530

    Enhance video upload with local save and notes generation
    Added functionality to save uploaded video locally, generate notes, and clean up temporary files.

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
 from fastapi import FastAPI, UploadFile, File, Form
+import shutil
+import os
+
+from database.upload import insert_video, insert_notes
 from s3_service import upload_video_to_s3
-from database.upload import insert_video
+from app.teacher.video_notes.notes_generator import generate_notes
 
 app = FastAPI()
 
 
+@app.get("/")
+def root():
+    return {"message": "Backend is running"}
+
 @app.post("/upload-video")
-async def upload_video(
+def upload_video(
     file: UploadFile = File(...),
     title: str = Form(...),
     subject: str = Form(...),
     teacher_id: int = Form(...),
     description: str = Form(None)
 ):
-    # 1. Upload video to S3
-    video_url = upload_video_to_s3(file)
 
-    # 2. Save metadata
-    insert_video(
-        title=title,
-        subject=subject,
-        video_url=video_url,
-        teacher_id=teacher_id,
-        description=description
-    )
+    # 1 save locally
+    temp_path = f"temp_{file.filename}"
 
-    return {
-        "message": "Video uploaded and saved successfully",
-        "video_url": video_url
-    }
+    with open(temp_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
 
+    # 2 upload to S3
+    video_url = upload_video_to_s3(temp_path)
 
+    # 3 save video → get id FIRST
+    video_id = insert_video(title, subject, video_url, teacher_id, description)
 
+    # 4 generate notes
+    generate_notes(temp_path, video_id)
+
+    # 5 delete temp
+    os.remove(temp_path)
+
+    return {"message": "Video + notes generated"}
